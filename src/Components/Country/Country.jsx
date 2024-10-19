aae75e9eeaddd5e7b61d1353831fcc8af4c34766
mohammaduzzal <aalp72142@gmail.com>
2024-10-20 05:21:41 +0600

    added the conditional css

--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './Country.css'
 
+
 const Country = ({country}) => {
     // console.log(country);
     const {name,flags,population,area,cca3} = country;
@@ -13,8 +14,8 @@ const Country = ({country}) => {
 
 
     return (
-        <div className='country'>
-            <h4>{name.common}</h4>
+        <div className={`country ${visited ? 'visited' : 'non-visited'}`}>
+            <h4 style={{color : visited? 'purple' : 'white'}}>{name.common}</h4>
             <img src={flags.png} alt="" />
             <p>population : {population}</p>
             <p>area : {area}</p>
